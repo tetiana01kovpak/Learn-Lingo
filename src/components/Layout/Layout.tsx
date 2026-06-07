765dc64b43384fb699722089a2eeb477e832d4df
Tetiana <tetianakovpak@gmail.com>
2026-06-07 14:43:59 +0300

    Fix home page styling inconsistencies

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,11 +1,18 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import clsx from 'clsx';
 import { Header } from '@/components/Header/Header';
+import styles from './Layout.module.css';
 
-export const Layout = () => (
-  <>
-    <Header />
-    <main style={{ flex: 1 }}>
-      <Outlet />
-    </main>
-  </>
-);
+export const Layout = () => {
+  const { pathname } = useLocation();
+  const isHome = pathname === '/';
+
+  return (
+    <div className={clsx(styles.shell, isHome ? styles.home : styles.default)}>
+      <Header />
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
